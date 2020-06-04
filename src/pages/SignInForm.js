1e669b34fe338adb092e548aba9da41dffeff237
swachchhandshrestha <swachchhandshrestha11@gmail.com>
2020-06-04 19:48:51 +0545

    handling an event testing

--- src/pages/SignInForm.js
+++ src/pages/SignInForm.js
@@ -1,19 +1,51 @@
 import React, {Component} from "react";
 
 class SignInForm extends Component{
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+
+    }
+
+        handleChange(e){
+            let target = e.target;
+            let value = target.type === 'checkbox' ? target.checked : target.value;
+            let name = target.name;
+
+            this.setState({
+                [name]: value
+            });
+        }
+
+        handleSubmit(e){
+        e.preventDefault();
+
+
+        console.log('This form was submitted with the following data:');
+        console.log(this.state);
+        }
+
+
     render(){
         return(
             <div className='FormCenter'>
-                <form className="FormFields" onSubmit={this.handleSubmit}>
+                <form  onSubmit = {this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
 
                     <div className="FormField">
                         <label className="FormField__Label" htmlFor="name">E-mail Address</label>
-                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email"/>
+                        <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange}/>
                     </div>
 
                     <div className="FormField">
                         <label className="FormField__Label" htmlFor="Password">Password</label>
-                        <input type="Password" id="Password" className="FormField__Input" placeholder="Enter your password" name="Password"/>
+                        <input type="Password" id="Password" className="FormField__Input" placeholder="Enter your password" name="Password" value={this.state.password} onChange={this.handleChange}/>
                     </div>
 
                     <div className="FormField">
